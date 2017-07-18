0c66de4a145728447886db797818a8f08ed5557b
STcompany <ilyakosolap@gmail.com>
2017-07-18 23:22:53 +0300

    ласт_коммит

--- copy/gulpfile.js
+++ copy/gulpfile.js
@@ -53,7 +53,7 @@ gulp.task('headersass', function() {
 gulp.task('libs', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		'app/libs/magnific-popup/dist/magnific-popup.min.js',
+		'app/libs/Magnific-Popup-master/dist/jquery.magnific-popup.min.js',
 		'app/libs/masked/jquery.maskedinput.min.js'
 		])
 		.pipe(concat('libs.min.js'))
